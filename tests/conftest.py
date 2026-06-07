e5e2e409ee74f4854df27f8d77462a16fa5d97d3
Bochra86 <mayboshra@gmail.com>
2026-06-07 19:17:06 -0300

    Fix GitHub Actions Python path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,18 @@
 from fastapi.testclient import TestClient
-from api.main import app
+from unittest.mock import MagicMock
 import pytest
+
+from api.main import app
+from api.database.dependencies import get_db
+
+
+def override_get_db():
+    db = MagicMock()
+    yield db
+
+
+app.dependency_overrides[get_db] = override_get_db
+
 
 client = TestClient(app)
 
